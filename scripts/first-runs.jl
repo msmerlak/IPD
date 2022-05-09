c46799259d0bf1037c2fee09e4789c289f808479
DrWatson <no@mail>
2022-05-09 15:53:04 +0200

    going back to this

---
+++ scripts/first-runs.jl
@@ -0,0 +1,187 @@
+
+
+using Distributed
+using DataFrames, Random, CSV, Dates
+
+
+mkdir(plotsdir("mixed", string(today())))
+mkdir(datadir("mixed", string(today())))
+
+PARALLEL = true
+PROCESSES = Sys.CPU_THREADS - 1
+PARALLEL && addprocs(PROCESSES)
+@everywhere begin
+    import Pkg
+    Pkg.activate(".")
+end
+@everywhere using DrWatson
+@everywhere begin
+    @quickactivate "IPD"
+    include("../src/utils.jl")
+    include("../src/IPD.jl")
+    include("../src/constants.jl")
+end
+
+include(srcdir("plotting.jl"))
+
+### different initial conditions
+for σ ∈ (1e-1),  n ∈ (100, 500), t ∈ (5, 10, 20), multiplicative ∈ (true, false)
+
+    println("σ = $σ, n = $n, t = $t, multiplicative = $multiplicative")
+
+    p = Dict(:RSTP => RSTP, :n => n, :t => t, :σ => σ, :multiplicative => multiplicative, :ϵ => 1e-10, :init_share => false, :m => 0.)
+
+    models = [create_model(p; rng = MersenneTwister(rand(UInt)), initial_strategy = rand(4)) for _ in 1:PROCESSES] 
+
+    adata, _ = ensemblerun!(models, player_step!, WF_sampling!, 5000, adata = [(:fitness, mean), (:strategy, mean), (:strategy, mean_std)], 
+    parallel = true)
+
+
+    q = subdict(p, [:n, :t, :σ, :multiplicative])
+
+    CSV.write(datadir("mixed", savename(q, "csv")), adata)
+
+    plot_strategies_1D(adata, plotsdir("mixed", string(today()), savename("1D", q, "png")))
+
+    plot_strategies_2D(adata, plotsdir("mixed", string(today()), savename("2D", q, "png")))
+
+    plot_strategies_3D(adata, plotsdir("mixed", string(today()), savename("3D", q, "png")))
+end
+
+
+
+### same initial conditions, different seeds (Parallel Evolution, PE)
+
+for σ ∈ (5e-3, 5e-2),  n ∈ (100, 500), t ∈ (5, 10), multiplicative ∈ (true, false)
+
+    println("σ = $σ, n = $n, t = $t, multiplicative = $multiplicative")
+
+    init = rand(4)
+
+    p = Dict(:RSTP => RSTP, :n => n, :t => t, :σ => σ, :multiplicative => multiplicative, :ϵ => 1e-10, :init_share => false, :m => 0.)
+
+    models = [create_model(p; rng = MersenneTwister(x), initial_strategy = init) for x in 1:PROCESSES] 
+
+    adata, _ = ensemblerun!(models, player_step!, WF_sampling!, 5000, adata = [(:fitness, mean), (:strategy, mean), (:strategy, mean_std)], 
+    parallel = true)
+
+    q = subdict(p, [:n, :t, :σ, :multiplicative])
+
+    CSV.write(datadir("mixed", string(today()), savename("PE", q, "csv")), adata)
+
+    plot_strategies_1D(adata, plotsdir("mixed", string(today()), savename("PE_1D", q, "png")))
+
+    plot_strategies_2D(adata, plotsdir("mixed", string(today()), savename("PE_2D", q, "png")))
+
+    plot_strategies_3D(adata, plotsdir("mixed", string(today()), savename("PE_3D", q, "png")))
+end
+
+
+### extortionary initial condition 
+
+σ, n, t = 5e-3, 500, 10
+init = [11/13, 1/2, 7/26, 0.]
+multiplicative = false
+
+
+p = Dict(:RSTP => RSTP, :n => n, :t => t, :σ => σ, :multiplicative => multiplicative, :ϵ => 1e-10, :init_share => false, :m => 0.)
+
+models = [create_model(p; rng = MersenneTwister(x), initial_strategy = init) for x in 1:PROCESSES] 
+
+adata, _ = ensemblerun!(models, player_step!, WF_sampling!, 20000, adata = [(:fitness, mean), (:strategy, mean), (:strategy, mean_std)], 
+parallel = true)
+
+q = subdict(p, [:n, :t, :σ, :multiplicative])
+
+CSV.write(datadir("mixed", string(today()), savename("PE_extortion", q, "csv")), adata)
+
+plot_strategies_1D(adata, plotsdir("mixed", string(today()), savename("PE_extortion_1D", q, "png")))
+
+plot_strategies_2D(adata, plotsdir("mixed", string(today()), savename("PE_extortion_2D", q, "png")))
+
+plot_strategies_3D(adata, plotsdir("mixed", string(today()), savename("PE_extortion_3D", q, "png")))
+
+### mean fitness
+
+σ = 5e-3
+n = 500
+t = 10
+multiplicative = false
+
+
+println("σ = $σ, n = $n, t = $t, multiplicative = $multiplicative")
+
+p = Dict(:RSTP => RSTP, :n => n, :t => t, :σ => σ, :multiplicative => multiplicative, :ϵ => 1e-10, :init_share => false, :m => 0.)
+
+models = [create_model(p; rng = MersenneTwister(rand(UInt)), initial_strategy = rand(4)) for _ in 1:PROCESSES] 
+
+adata, _ = ensemblerun!(models, player_step!, WF_sampling!, 5000, adata = [(:fitness, mean), (:strategy, mean), (:strategy, mean_std), (:share, mean)], 
+parallel = true)
+
+q = subdict(p, [:n, :t, :σ, :multiplicative, :m])
+
+
+plot_mean_fitness(adata, plotsdir("mixed", string(today()), savename("FITNESS", q, "png")))
+
+### extorsion/generosity
+
+σ = 5e-3
+n = 500
+t = 10
+multiplicative = false
+
+println("σ = $σ, n = $n, t = $t, multiplicative = $multiplicative")
+
+p = Dict(:RSTP => RSTP, :n => n, :t => t, :σ => σ, :multiplicative => multiplicative, :ϵ => 1e-10, :init_share => false, :m => 0.)
+
+models = [create_model(p; rng = MersenneTwister(rand(UInt)), initial_strategy = rand(4)) for _ in 1:PROCESSES] 
+
+adata, _ = ensemblerun!(models, player_step!, WF_sampling!, 10000, adata = [(:fitness, mean), (:strategy, mean), (:cooperation, mean), (:generosity, mean), (:extorsion, mean)], 
+parallel = true)
+
+q = subdict(p, [:n, :t, :σ, :multiplicative, :m])
+
+for Y ∈ (:mean_cooperation, :mean_extorsion, :mean_generosity, :mean_fitness)
+
+    plot_property(adata, Y, plotsdir("mixed", string(today()), savename(string(Y), q, "png")))
+end
+
+
+
+
+
+### save whole population
+
+σ = 5e-3
+n = 500
+t = 10
+multiplicative = false
+
+
+println("σ = $σ, n = $n, t = $t, multiplicative = $multiplicative")
+
+p = Dict(:RSTP => RSTP, :n => n, :t => t, :σ => σ, :multiplicative => multiplicative, :ϵ => 1e-10, :init_share => false, :m => 0.)
+
+models = [create_model(p; rng = MersenneTwister(rand(UInt)), initial_strategy = rand(4)) for _ in 1:PROCESSES] 
+
+adata, _ = ensemblerun!(models, player_step!, WF_sampling!, 5000, adata = [(:fitness, mean), (:strategy, mean_std)], 
+parallel = true)
+
+q = subdict(p, [:n, :t, :σ, :multiplicative, :m])
+
+Y = :mean_std_strategy
+
+plot_property(adata, Y, plotsdir("mixed", string(today()), savename(string(Y), q, "png")))
+
+### entropy
+
+σ, n, t = 5e-3, 500, 10
+init = rand(4)
+multiplicative = false
+
+
+p = Dict(:RSTP => RSTP, :n => n, :t => t, :σ => σ, :multiplicative => multiplicative, :ϵ => 1e-10, :init_share => false, :m => 0.)
+
+model = create_model(p, rng = MersenneTwister())
+
+adata, mdata = run!(model, player_step!, WF_sampling!, 5000, adata = [:strategy])
---
+++ src/ABM-reactive.jl
@@ -0,0 +1,143 @@
+using DrWatson
+
+include(srcdir("memory-one-IPD.jl"))
+include(srcdir("sample-with-LOD.jl"))
+include(srcdir("constants.jl"))
+
+using Agents
+using Distributions: Normal, Bernoulli
+using Random: GLOBAL_RNG
+using StatsBase: mean
+using StaticArrays
+
+
+mutable struct Mem1Player <: AbstractAgent
+    id::Int
+    pos::NTuple{2,Int}
+    strategy::MVector{2, Float64}
+    scores::Vector{Float64}
+    fitness::Float64
+    LOD::Vector{Int64}
+end
+
+function create_model(
+    p;
+    space = nothing,
+    compute_metrics = false,
+    LOD = false,
+    RSTP = RSTP,
+    rng = GLOBAL_RNG
+)
+
+    properties = deepcopy(p)
+
+    properties[:compute_metrics] = compute_metrics
+    properties[:LOD] = LOD
+    properties[:RSTP] = RSTP
+    properties[:selection] = a -> mean(a.scores)
+
+    model = AgentBasedModel(Mem1Player, space, properties = properties, rng = rng)
+    model.n = Int(model.n)
+
+
+    if model.space === nothing
+        for id = 1:model.n
+            add_agent!(
+                Mem1Player(
+                    id,
+                    (1, 1),
+                    model.init_strategy,
+                    Float64[1.0],
+                    0.0,
+                    Int64[],
+                    0.0,
+                    0.0,
+                    0.0,
+                ),
+                model,
+            )
+        end
+    else
+        fill_space!(
+            model,
+            model.init_strategy,
+            Float64[1.0],
+            0.0,
+            Int64[],
+            0.0,
+            0.0,
+            0.0,
+        )
+    end
+    return model
+end
+
+
+function match!((X, Y)::Tuple{Mem1Player,Mem1Player}, model)
+
+    ## Press-Dyson formula for stationary payoffs
+    if X.id != Y.id
+        push!(X.scores, π(X.strategy, Y.strategy, model.RSTP))
+        push!(Y.scores, π(Y.strategy, X.strategy, model.RSTP))
+    end
+end
+
+
+function play_matches!(player, model)
+
+    ## pick competitors
+    if model.space === nothing
+        competitors = allagents(model)
+    else
+        competitors = nearby_agents(player, model)
+    end
+
+    ## play matches
+    for competitor in competitors
+        match!((player, competitor), model)
+    end
+
+    ## compute metrics
+    if model.compute_metrics
+        player.cooperation = cooperation(player, competitors)
+        #player.generosity = generosity(player, competitors)
+        #player.extorsion = extorsion(player, competitors)
+    end
+end
+
+
+function mutate!(player, model)
+    jiggle!(player.strategy, model.σ, model.rng)
+    window!(player.strategy)
+end
+
+function jiggle!(x::MVector{4}, σ, rng)
+    x .+= σ * (2*rand(rng, MVector{2}) .- 1)
+end
+
+function window!(x)
+    @. x = min(max(x, 1e-9), 1.0 - 1e-9)
+end
+
+function player_step!(player, model)
+    mutate!(player, model)
+    play_matches!(player, model)
+end
+
+function WF_sampling!(model)
+
+    # compute fitness and reset scores etc
+    for a in allagents(model)
+        a.fitness = model.selection(a)
+        a.scores = Float64[]
+        sizehint!(a.scores, model.n^2)
+    end
+
+    # Wright-Fisher sampling
+    if !model.LOD
+        Agents.sample!(model, model.n, :fitness)
+    else
+        sample_with_LOD!(model, model.n, :fitness)
+    end
+
+end
